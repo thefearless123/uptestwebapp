1a6150b034a70be3735b0304ebebd11d80bbc212
Udayaprakash <udayaprakash123@gmail.com>
2022-12-11 21:44:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const { url } = require('inspector');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 80;
 let indexPage = (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
